25a5d772fc826935a6f10237afb3b9c32b174ef2
MIN <myoping99@gmail.com>
2024-02-06 14:25:46 +0900

    Update Day03.java

--- Java_programmers/Day03.java
+++ Java_programmers/Day03.java
@@ -3,7 +3,7 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         for(int i = 0; i < str1.length(); i++){
-            answer+ = String.valueOf(str1.charAt(i))+str2.charAt(i);//String.valueOf는 String으로 형변환 하는 것?
+            answer+ = String.valueOf(str1.charAt(i))+str2.charAt(i);//String.valueOf는 변환해주는 것
         }
         
         return answer;

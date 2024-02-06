1de4afdcc906c440529504a559e5b3b2ed363c26
MIN <myoping99@gmail.com>
2024-02-06 14:27:52 +0900

    Update Day03.java

--- Java_programmers/Day03.java
+++ Java_programmers/Day03.java
@@ -9,3 +9,16 @@ class Solution {
         return answer;
     }
 }
+
+*문자 리스트를 문자열로 변환하기
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for(int i = 0; i < arr.length; i++){
+        answer += arr[i];
+        }
+        return answer;
+    }
+}
+
+*
